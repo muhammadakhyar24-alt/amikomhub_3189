800743548c04919b8d0e911f9cf4566e23b09974
Muhammad Akhyar <emailkamu@gmail.com>
2026-05-12 14:03:27 +0700

    Tambah menu Manajemen Partner di sidebar admin layout

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -17,6 +17,7 @@
             <nav class="space-y-4">
                 <a href="{{ route('admin.dashboard') }}" class="block px-4 py-2 rounded hover:bg-indigo-600 transition">Dashboard</a>
                 <a href="{{ route('admin.events.index') }}" class="block px-4 py-2 rounded hover:bg-indigo-600 transition">Manajemen Event</a>
+                <a href="{{ route('admin.partners.index') }}" class="block px-4 py-2 rounded hover:bg-indigo-600 transition">Manajemen Partner</a>
             </nav>
         </div>
 
